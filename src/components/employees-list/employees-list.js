4fc29154606041ea48c494a458e8345b2ae18abe
Binqie <shamir.beishebaev@icloud.com>
2022-02-23 22:32:33 +0600

    Key was added

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,16 @@
 import './employees-list.css';
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 
-export default function EmployeesList() {
+export default function EmployeesList({data}) {
+
+    const elements = data.map((item) => {
+        const {id, ...itemProps} = item;
+        return <EmployeesListItem {...itemProps} key={id}/>
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     )
 }
