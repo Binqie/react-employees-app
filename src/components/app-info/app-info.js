daaa85468c6dc86794913f2d3d47108acfc17c7f
Binqie <shamir.beishebaev@icloud.com>
2022-02-25 14:35:26 +0600

    show app header info

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,14 +1,11 @@
-import { Component } from 'react';
 import './app-info.css';
 
-export default class AppInfo extends Component {
-    render() {
-        return (
-            <div className="app-info">
-                <h1>Учет сотрудников в компании N</h1>
-                <h2>Общее число сотрудников: </h2>
-                <h3>Премию получат: </h3>
-            </div>
-        )
-    }
+export default function AppInfo({allEmployees, increasedEmployees}) {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании N</h1>
+            <h2>Общее число сотрудников: {allEmployees}</h2>
+            <h3>Премию получат: {increasedEmployees}</h3>
+        </div>
+    )
 }
