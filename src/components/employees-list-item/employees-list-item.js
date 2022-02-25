df7594fda58c09f7e3ac2328a1e33b779bd2f21d
Binqie <shamir.beishebaev@icloud.com>
2022-02-25 14:37:31 +0600

    delete, rise, increase employees

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,57 +1,33 @@
-import { Component } from 'react';
-
 import './employees-list-item.css'
 
-export default class EmployeesListItem extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            like: false,
-        }
-    }
+export default function EmployeesListItem({name, salary, onDelete, onToggleProp, increase, rise}) {
+    const classList = "list-group-item d-flex justify-content-between" + (increase ? ' increase' : '') + (rise ? ' like' : '');
 
-    onIncrease = () => {
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
+    return (
+        <li className={classList}>
+            <span 
+                className="list-group-item-label"
+                data-toggle="rise"
+                onClick={onToggleProp}>{name}</span>
 
-    onRise = () => {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
 
-    render() {
-        const {name, salary} = this.props;
-        const {increase, like} = this.state;
-        const classList = "list-group-item d-flex justify-content-between" + (increase ? ' increase' : '') + (like ? ' like' : '');
-
-        return (
-            <li className={classList}>
-                <span 
-                    className="list-group-item-label"
-                    onClick={this.onRise}>{name}</span>
-
-                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-
-                <div className="d-flex justify-content-center align-items-center">
-                    <button 
-                        type='button'
-                        className="btn-cookie btn-sm"
-                        onClick={this.onIncrease}>
-                        <i className="fas fa-cookie"></i>
-                    </button>
-                    <button 
-                        type='button'
-                        className="btn-trash btn-sm">
-                        <i className="fas fa-trash"></i>
-                    </button>
-                    <i className="fas fa-star"></i>
-                </div>
-            </li>
-        )
-    }
+            <div className="d-flex justify-content-center align-items-center">
+                <button 
+                    type='button'
+                    className="btn-cookie btn-sm"
+                    data-toggle="increase"
+                    onClick={onToggleProp}>
+                    <i className="fas fa-cookie"></i>
+                </button>
+                <button 
+                    type='button'
+                    className="btn-trash btn-sm"
+                    onClick={onDelete}>
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    )
 }
-
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,23 +1,20 @@
-import { Component } from 'react';
 import './employees-list.css';
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 
-export default class EmployeesList extends Component {
-    constructor(props) {
-        super(props);
-        this.data = props.data;
-    }
+export default function EmployeesList({data, onDelete, onToggleProp}) {
+    const elements = data.map((item) => {
+        const {id, ...itemProps} = item;
+        return <EmployeesListItem 
+                    {...itemProps}
+                    key={id}
+                    onDelete={() => onDelete(id)}
+                    onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>
+    })
 
-    render() {
-        const elements = this.data.map((item) => {
-            const {id, ...itemProps} = item;
-            return <EmployeesListItem {...itemProps} key={id}/>
-        })
-    
-        return (
-            <ul className="app-list list-group">
-                {elements}
-            </ul>
-        )
-    }
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+
 }
