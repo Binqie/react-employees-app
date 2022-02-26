827b204f647aee3a2c709d20e89b834301e0c458
Binqie <shamir.beishebaev@icloud.com>
2022-02-26 14:53:54 +0600

    search employees function was added

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -2,12 +2,27 @@ import { Component } from 'react'
 import './search-panel.css'
 
 export default class SearchPanel extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        }
+    }
+
+    onUpdateSearch = (e) => {
+        this.setState({term: e.target.value})
+        this.props.onUpdateSearch(e.target.value)
+    }
+
     render() {
         return (
             <input 
                 type="text"
                 className="form-control search-input"
-                placeholder="Найти сотрудника"/>
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearch}/>
         )
     }
 }
